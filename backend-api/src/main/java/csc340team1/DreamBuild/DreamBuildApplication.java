20570edf9c54c20138f00023cef959f4c467aec7
tutruong8 <travistu8@gmail.com>
2025-10-22 10:51:51 -0400

    backend api initial commit

---
+++ backend-api/src/main/java/csc340team1/DreamBuild/DreamBuildApplication.java
@@ -0,0 +1,13 @@
+package csc340team1.DreamBuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamBuildApplication.class, args);
+	}
+
+}
---
+++ backend-api/src/test/java/csc340team1/DreamBuild/DreamBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package csc340team1.DreamBuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

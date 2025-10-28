a56a446feb08c81672fe58ee818ac59abf09c40e
tutruong8 <travistu8@gmail.com>
2025-10-28 10:07:45 -0400

    forgot to put @RestController in review

--- backend-api/src/main/java/csc340team1/DreamBuild/review/ReviewController.java
+++ backend-api/src/main/java/csc340team1/DreamBuild/review/ReviewController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;

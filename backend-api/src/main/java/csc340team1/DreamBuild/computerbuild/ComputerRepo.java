1f77e6a13fb3028f125bd0a15155f2267bbb9d97
Bryan Palma <supersky64ofderps@gmail.com>
2025-12-11 00:26:23 -0500

    KILL MYSELF

--- backend-api/src/main/java/csc340team1/DreamBuild/computerbuild/ComputerRepo.java
+++ backend-api/src/main/java/csc340team1/DreamBuild/computerbuild/ComputerRepo.java
@@ -1,4 +1,4 @@
-package csc340team1.DreamBuild.computerBuild;
+package csc340team1.DreamBuild.computerbuild;
 import java.util.*;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
